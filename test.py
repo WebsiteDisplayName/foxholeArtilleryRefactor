fa72beb611ac11524a9653e71e361ee1e0f398b9
WebsiteDisplayName <rowemg@bc.edu>
2023-01-28 23:50:49 -0500

    mouse area screen cap

--- test.py
+++ test.py
@@ -1,17 +1,37 @@
-import dearpygui.dearpygui as dpg
+# Mouse location: https://docs.google.com/document/d/1el2fGCOhXFpR9ADOG8K1Ih_GNmPv29dH9NMt0LJZlN4/edit
+# Screenshot area
 
-dpg.create_context()
 
-dpg.show_documentation()
-dpg.show_style_editor()
-dpg.show_debug()
-dpg.show_about()
-dpg.show_metrics()
-dpg.show_font_manager()
-dpg.show_item_registry()
 
-dpg.create_viewport(title='Custom Title', width=800, height=600)
-dpg.setup_dearpygui()
-dpg.show_viewport()
-dpg.start_dearpygui()
-dpg.destroy_context()
+
+#! python3
+import pyautogui
+import sys
+import keyboard
+import PIL
+import os
+
+# -50, 0, 100, 100
+# small box around cursor
+def screenshotMouseArea(fileName, left = -50, top = 0, width = 100, height = 100):
+    # left, top, width, height
+    x, y = pyautogui.position()
+    positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+    print(positionStr, end='')
+    print('\b' * len(positionStr), end='', flush=True)
+
+    # adjust coords
+    left += x
+    top += y
+
+    # file path adjustments
+    im = pyautogui.screenshot(region=(left, top, width, height))
+    script_dir = os.path.dirname(__file__)
+    rel_path = "images\\" + fileName
+    abs_file_path = os.path.join(script_dir, rel_path)
+    im.save(abs_file_path)
+
+while True:
+    if keyboard.is_pressed("right alt"):
+        screenshotMouseArea("hehexd.jpg")
+
