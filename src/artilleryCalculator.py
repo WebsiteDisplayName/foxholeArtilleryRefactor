af03d0ed251c6107e0307c05cb2790f0f5ed71ba
WebsiteDisplayName <rowemg@bc.edu>
2023-01-30 15:26:23 -0500

    added wind methods

--- src/artilleryCalculator.py
+++ src/artilleryCalculator.py
@@ -11,12 +11,24 @@ class firingSolution:
     spotterToTargetDistance = 0
     spotterToGunAzimuth = 0
     spotterToGunDistance = 0
+    windAzimuth = 0
+    windForce = 1
+    weaponType = 1  # 120mm & 150mm
 
     def spotterSwitch(self):
         if self.spotter == False:
             self.spotter = True
         else:
             self.spotter = False
+            self.spotterToGunAzimuth = 0
+            self.spotterToGunDistance = 0
+
+    def updateWind(self, windAzimuth, windForce):  # wf = 1-3
+        self.windAzimuth = windAzimuth
+        self.windForce = windForce
+
+    def updateWeapon(self, weaponType):  # wt = 1-3
+        self.weaponType = weaponType
 
     pass
 
